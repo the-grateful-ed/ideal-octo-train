15d654640912d9bc576e38411c6bba5595471311
ed <edndacomputer@riseup.net>
2023-10-20 05:08:26 -0700

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,25 @@
 import Link from "next/link"
+
 import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
+import { Poppins, Ubuntu } from "next/font/google"
+
 import { Analytics } from "@/components/analytics"
-import { ModeToggle } from "@/components/mode-toggle"
-import Header from "@/components/header"
-import { Ubuntu, Poppins } from 'next/font/google';
 import Footer from "@/components/footer"
+import Header from "@/components/header"
+import { ModeToggle } from "@/components/mode-toggle"
+import { ThemeProvider } from "@/components/theme-provider"
 
-const ubuntu = Ubuntu({ 
-  subsets: ['latin'],
-  weight:["400", "700"],
-  variable: '--font-ubuntu',
-});
+const ubuntu = Ubuntu({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-ubuntu",
+})
 
-const poppins = Poppins({ 
-  subsets: ['latin'],
-  weight:["400", "700"],
-  variable: '--font-poppins',
-});
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-poppins",
+})
 
 export const metadata = {
   title: "edndacomputer",
@@ -27,24 +29,20 @@ export const metadata = {
 interface RootLayoutProps {
   children: React.ReactNode
 }
-
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body
-        className={`min-h-screen bg-background text-slate-900 antialiased dark:text-slate-50 ${ubuntu.variable} ${poppins.variable}`}>
+        className={`min-h-screen bg-background text-slate-900 antialiased dark:text-slate-50 ${ubuntu.variable} ${poppins.variable}`}
+      >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <div className="mx-auto max-w-2xl px-4 py-10">
-          <Header header="edndacomputer" subheader="a blog about computers and the current state of the internet." /> 
-                       <div className="flex items-center justify-between">
-                <ModeToggle />
-                <nav className="ml-auto space-x-6 text-sm font-medium">
-                  <Link href="/">Home</Link>
-                  <Link href="/about">About</Link>
-                </nav>
-              </div>
-            <main>{children}</main>
-        <Footer />
+          <div className="mx-auto flex min-h-screen max-w-2xl flex-col px-4 pt-10">
+            <Header
+              header="edndacomputer"
+              subheader="a blog about computers and the current state of the internet."
+            />
+            <main className="flex grow flex-col">{children}</main>
+            <Footer />
           </div>
           <Analytics />
         </ThemeProvider>
